d235d529e072220308e1c3b3be81397c792af658
kzok <yes.notyet@gmail.com>
2023-06-11 11:50:06 +0900

    add remount app button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,47 @@
+import { useState } from "react";
 import { ControlSection } from "./control-section";
+import styles from "./index.module.scss";
 import { usePageInitialization } from "./store";
 import { TodoList } from "./todo-list";
 
-export const Page: React.FC = () => {
+const AppContent: React.FC = () => {
   const status = usePageInitialization();
   if (status === "pending") {
     return <p>loading</p>;
   }
   return (
     <>
-      <h1>An example TODO app</h1>
       <hr />
       <ControlSection />
       <hr />
       <TodoList />
+    </>
+  );
+};
+
+export const Page: React.FC = () => {
+  const [mount, setMount] = useState(true);
+  const reset = async () => {
+    if (!mount) {
+      return;
+    }
+    setMount(false);
+    await new Promise<void>((done) => setTimeout(done, 100));
+    setMount(true);
+  };
+  return (
+    <>
+      <div className={styles["container"]}>
+        <h1>An example TODO App</h1>
+        <button
+          disabled={!mount}
+          onClick={reset}
+          title="remount app to test cleanup"
+        >
+          remount App
+        </button>
+      </div>
+      {mount && <AppContent />}
     </>
   );
 };
